865136b6fa2f40ad9ab27134f0974a785ede3d17
Ben <uhavemyword@users.noreply.github.com>
2018-05-15 00:38:46 +0800

    close channel on each schedule.

--- jiaguo-consumer/src/main/java/com/cp/jiaguo/consumer/Worker.java
+++ jiaguo-consumer/src/main/java/com/cp/jiaguo/consumer/Worker.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeoutException;
 
 @Component
@@ -24,35 +25,66 @@ public class Worker {
     private Config config;
 
     @PostConstruct
-    public void init() throws IOException, TimeoutException {
+    public void init() {
         rabbitFactory = new ConnectionFactory();
         rabbitFactory.setHost(config.getRabbitHost());
         rabbitFactory.setPort(config.getRabbitPort());
         rabbitFactory.setUsername(config.getRabbitUser());
         rabbitFactory.setPassword(config.getRabbitPassword());
-        connection = rabbitFactory.newConnection();
-        channel = connection.createChannel();
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    }
+
+    private void ensureChannelOpen() {
+        try {
+            if (connection == null || !connection.isOpen()) {
+                connection = rabbitFactory.newConnection();
+            }
+            if (channel == null || !channel.isOpen()) {
+                channel = connection.createChannel();
+            }
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            log.debug("ensureChannelOpen() success.");
+        } catch (Exception ex) {
+            log.error("ensureChannelOpen() failed.", ex);
+        }
     }
 
     @Scheduled(fixedRateString = "${my.task.interval}")
-    public void doWork() throws IOException {
+    public void doWork() throws IOException, InterruptedException, TimeoutException {
+        log.debug("Start doWork().");
+        ensureChannelOpen();
+
+        int messageCount = (int) channel.messageCount(QUEUE_NAME);
+        log.debug("{} message found in the {} queue.", messageCount, QUEUE_NAME);
+        if (messageCount < 1) {
+            return;
+        }
+
+        CountDownLatch latch = new CountDownLatch(messageCount);
+
         final Consumer consumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope,
                                        AMQP.BasicProperties properties, byte[] body) throws IOException {
-                String message = new String(body, "UTF-8");
                 try {
-                    resultHandler.save(message);
-                } catch (Exception ex) {
-                    log.error("Error when save the message: " + message, ex);
+                    String message = new String(body, "UTF-8");
+                    try {
+                        resultHandler.save(message);
+                    } catch (Exception ex) {
+                        log.error("Error when save the message: " + message, ex);
+                    } finally {
+                        channel.basicAck(envelope.getDeliveryTag(), false);
+                    }
                 } finally {
-                    channel.basicAck(envelope.getDeliveryTag(), false);
+                    latch.countDown();
                 }
             }
         };
 
         boolean autoAck = false;
         channel.basicConsume(QUEUE_NAME, autoAck, consumer);
+
+        latch.await();
+        // in real world, it's better to keep a long lived channel rather than closing it.
+        channel.close();
     }
 }
