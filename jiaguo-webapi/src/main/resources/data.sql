7f7a1dfb5022d74df220c7e4fd7520cecbde3686
Ben <uhavemyword@users.noreply.github.com>
2018-05-14 22:19:15 +0800

    add column creation_time

--- jiaguo-webapi/src/main/resources/data.sql
+++ jiaguo-webapi/src/main/resources/data.sql
@@ -4,25 +4,29 @@ use mytest;
 
 CREATE TABLE `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NULL,
+  `creation_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NULL,
   PRIMARY KEY (`id`));
 
 CREATE TABLE `contact` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `creation_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_id` INT NOT NULL,
-  `phone` VARCHAR(45) NULL,
-  `address` VARCHAR(45) NULL,
+  `phone` VARCHAR(50) NULL,
+  `address` VARCHAR(255) NULL,
   PRIMARY KEY (`id`));
 
 CREATE TABLE `favorite` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `url` VARCHAR(45) NULL,
+  `creation_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` VARCHAR(255) NOT NULL,
+  `url` VARCHAR(1000) NULL,
   PRIMARY KEY (`id`));
 
 CREATE TABLE `product` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `external_id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `creation_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `external_id` int NOT NULL,
   `info_json` json NOT NULL,
-  PRIMARY KEY (`id`);
+  PRIMARY KEY (`id`));
