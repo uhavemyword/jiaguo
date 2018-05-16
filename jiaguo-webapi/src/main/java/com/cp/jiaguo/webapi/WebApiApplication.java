8fe55a7bc4deca8c9dbe4db7e89d2342181b5326
Ben <peng-chen@live.com>
2018-05-16 17:03:33 +0800

    involve redis

--- jiaguo-webapi/src/main/java/com/cp/jiaguo/webapi/WebApiApplication.java
+++ jiaguo-webapi/src/main/java/com/cp/jiaguo/webapi/WebApiApplication.java
@@ -2,16 +2,32 @@ package com.cp.jiaguo.webapi;
 
 import org.mybatis.spring.annotation.MapperScan;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 @SpringBootApplication
 @MapperScan("com.cp.jiaguo.webapi.dao")
 @EnableTransactionManagement
 public class WebApiApplication {
+    @Autowired
+    private ApplicationContext appContext;
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebApiApplication.class, args);
+    }
+
+    @Bean
+    public JedisPool jedisPool() {
+        Environment env = appContext.getEnvironment();
+        String jedisHost = env.getProperty("spring.redis.host");
+        return new JedisPool(new JedisPoolConfig(), jedisHost);
+    }
 }
--- jiaguo-webapi/src/main/java/com/cp/jiaguo/webapi/controller/DashboardController.java
+++ jiaguo-webapi/src/main/java/com/cp/jiaguo/webapi/controller/DashboardController.java
@@ -3,15 +3,23 @@ package com.cp.jiaguo.webapi.controller;
 import com.cp.jiaguo.webapi.model.DashboardModel;
 import com.cp.jiaguo.webapi.service.ProductService;
 import com.cp.jiaguo.webapi.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 @RestController
 @RequestMapping("/api/dashboard")
 public class DashboardController {
+    private static final Logger log = LoggerFactory.getLogger(DashboardController.class);
+    private static final String CACHE_KEY = "productCount";
+
+    @Autowired
+    private JedisPool jedisPool;
     @Autowired
     private UserService userService;
     @Autowired
@@ -19,9 +27,29 @@ public class DashboardController {
 
     @GetMapping("/demo")
     public DashboardModel getDemo() {
+
         DashboardModel model = new DashboardModel();
-        model.setProductCount(productService.getCount());
+        model.setProductCount(Long.valueOf(getProductCountWithCache()));
         model.setUesrCount(userService.getCount());
         return model;
+    }
+
+    private String getProductCountWithCache() {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+        } catch (Exception ex) {
+            log.error("Failed to get resource from Jedis pool.", ex);
+        }
+
+        if (jedis != null) {
+            String value = jedis.get(CACHE_KEY);
+            if (value == null) {
+                value = productService.getCount().toString();
+                jedis.setex(CACHE_KEY, 60, value);
+            }
+            return value;
+        }
+        return productService.getCount().toString();
     }
 }
